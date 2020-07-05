f8a5c6a929b2210ce0083907fba5070a8ce219df
Milan Lesichkov <lesichkovm@gmail.com>
2020-07-05 10:46:21 +0100

    Create api.go

---
+++ api.go
@@ -0,0 +1,28 @@
+package api
+
+// Response defines an response for the API
+type Response struct {
+	Status  string                 `json:Status`
+	Message string                 `json:Message`
+	Data    map[string]interface{} `json:Data`
+}
+
+// Error returns an error message
+func Error(message string) Response {
+	return Response{Status: "error", Message: message, Data: map[string]interface{}{}}
+}
+
+// ErrorData returns an error message with data
+func ErrorData(message string, data map[string]interface{}) Response {
+	return Response{Status: "error", Message: message, Data: data}
+}
+
+// Success returns a success message
+func Success(message string) Response {
+	return Response{Status: "success", Message: message, Data: map[string]interface{}{}}
+}
+
+// SuccessData returns a success message with data
+func SuccessData(message string, data map[string]interface{}) Response {
+	return Response{Status: "success", Message: message, Data: data}
+}
